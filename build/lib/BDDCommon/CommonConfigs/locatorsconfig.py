b0e9b5da90fe2c2c511eea23d27333304762fdba
richa jha <richajha1154@gmail.com>
2021-02-09 09:33:15 +0000

    deleting a folder

--- build/lib/BDDCommon/CommonConfigs/locatorsconfig.py
+++
@@ -1,7 +0,0 @@
-
-
-LOCATORS = {
-    'main navigation' : {'type': 'id', 'locator':'mainnav'},
-    'top navigation' : {'type': 'id', 'locator':'top'},
-    'options' : {'type': 'css selector', 'locator':'.options-bar'}
-}
